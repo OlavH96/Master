1581b9da45f22a7f3370600940df8be8c573f1ed
Olav Reppe Husby <OlavH96@gmail.com>
2019-10-14 16:17:00 +0200

    Testing tf object_detection framework

---
+++ sign_detection/image_generation/label_gen.py
@@ -0,0 +1,21 @@
+import os
+
+def get_labels():
+    SIGN_PATH = 'image_generation/signs'
+    names = os.listdir(SIGN_PATH)
+    return sorted([path for path in names if os.path.isdir(os.path.join(SIGN_PATH, path))])
+
+
+def gen_labelmap():
+    template = \
+"""item {
+    id: %r
+    name: '%s'
+}"""
+    content = '\n\n'.join([template % (i+1, name) for i, name in enumerate(get_labels())])
+    with open('labelmap.pbtxt', 'w') as f:
+        f.write(content + '\n')
+
+
+if __name__ == '__main__':
+    gen_labelmap()
